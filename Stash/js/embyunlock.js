71973f315e5dc695e97ff253c2aeecdd2189bf1d
PsychoPasss <x308989414@gmail.com>
2024-02-19 15:52:15 +0800

    Update embyunlock.js

--- Stash/js/embyunlock.js
+++ Stash/js/embyunlock.js
@@ -4,11 +4,23 @@ if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice
         $done({
             status: 200,
             headers: $response.headers,
-            body: '{"cacheExpirationDays":360,"resultCode":"GOOD","message":"Device Valid"}'
+            body: '{"cacheExpirationDays":365,"resultCode":"GOOD","message":"Device Valid"}'
         })
     } else {
         $done({})
     }
+} else if ($request.url.indexOf('mb3admin.com/admin/service/registration/getStatus') != -1) {
+    $done({
+            status: 200,
+            headers: $response.headers,
+            body: '{"deviceStatus":"0","planType":"Lifetime","subscriptions":{}}'
+        })
+} else if ($request.url.indexOf('mb3admin.com/admin/service/registration/validate') != -1) {
+    $done({
+            status: 200,
+            headers: $response.headers,
+            body: '{"featId":"","registered":true,"expDate":"2099-01-01","key":""}'
+        })
 } else {
     $done({})
 }
