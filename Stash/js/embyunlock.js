ac0e555a1d1db0d96ad054df29ed3e9f05288397
PsychoPasss <x308989414@gmail.com>
2024-02-19 13:58:20 +0800

    Update embyunlock.js

--- Stash/js/embyunlock.js
+++ Stash/js/embyunlock.js
@@ -1,6 +1,6 @@
 if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice') != -1) {
     if ($response.status != 200) {
-        // $notification.post("Done","Success","Emby Premiere Unlocked!");
+        $notification.post("Done","Success","Emby Premiere Unlocked!");
         $done({
             status: 200,
             headers: $response.headers,
